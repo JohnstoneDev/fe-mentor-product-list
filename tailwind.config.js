a63d106d0771689a3bc66286b6597e7b04845a58
Marucha Johnstone <marucha.webdev@gmail.com>
2024-11-11 11:43:59 +0300

    styles : Configure fonts & colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],  theme: {
     fontFamily : {
-      global : ['Inter', 'sans-serif'],
+      global : ['Red Hat Text', 'sans-serif'],
     },
     fontSize : {
       'heading' : ['30px', {
@@ -13,13 +13,7 @@ module.exports = {
           fontWeight : '700',
         }
       ],
-    'section' : ['20px', {
-        lineHeight : '32px',
-        letterSpacing : '0px',
-        fontWeight : '500',
-      }
-     ],
-    'regular' : ['15px', {
+    'regular' : ['16px', {
         lineHeight : '28px',
         letterSpacing : '0px',
         fontWeight : '400',
@@ -31,6 +25,16 @@ module.exports = {
       'tablet': '640px',
       'laptop': '1024px',
       'desktop': '1280px',
+    },
+    colors : {
+      'red'      : 'hsl(14, 86%, 42%)',
+      'green'    : 'hsl(159, 69%, 38%)',
+      'rose-50'  : 'hsl(20, 50%, 98%)',
+      'rose-100' : 'hsl(13, 31%, 94%)',
+      'rose-300' : 'hsl(14, 25%, 72%)',
+      'rose-400' : 'hsl(7, 20%, 60%)',
+      'rose-500' : 'hsl(12, 20%, 44%)',
+      'rose-900' : 'hsl(14, 65%, 9%)',
     },
     extend: {
     },
